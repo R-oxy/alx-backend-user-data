5b99f982b9de909650368f71d6f1306cae8c5c2f
R-oxy <aronmangati@gmail.com>
2024-07-04 01:34:02 -0700

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-Module for obfuscating log messages
+Module for obfuscating log messages and custom log formatter
 """
 
+import logging
 import re
 from typing import List
 
@@ -15,3 +16,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
     return re.sub(
         pattern, lambda m: f'{m.group().split("=")[0]}={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats log records to filter PII fields.
+        """
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original,
+                            self.SEPARATOR)
