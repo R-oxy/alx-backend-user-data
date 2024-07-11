70937d03bb0243db1d95b0a6c42aad3fd36e33d7
R-oxy <aronmangati@gmail.com>
2024-07-11 13:56:41 +0300

    logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,12 +32,12 @@ def login():
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user = users[0]
-
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
+    user = users[0]
 
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
